15d852508b03e48b6f767c74702716f138ada914
github username <github email address>
2019-10-06 15:37:32 -0500

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
-
+const Todo = props => <li>{props.text} <span><button onClick={() => props.delete(props.todo.id)}>Delete</button></span></li>
+//props.text is passed from the dispatch to the reducer
 export default Todo;
+ 
--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,7 +4,7 @@ import Todo from './Todo'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+  renderTodos = () => this.props.todos.map((todo, id) => <Todo delete={this.props.delete} key={todo.id} text={todo} />)
 
   render() {
     return(
@@ -14,11 +14,16 @@ class TodosContainer extends Component {
     );
   }
 };
-
+ 
+mapDispatchToProps = dispatch => {
+  return {
+    delete: todoText => dispatch({type: 'DELETE_TODO', payload: todoText})  //this.props.delete
+  }
+}
 mapStateToProps = state => {
   return {
-    todos: state.todos
+    todos: state.todos  //naming the props here called todos so that it can be this.props.todos
   }
 }
 
-export default connect(mapStateToProps)(TodosContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
